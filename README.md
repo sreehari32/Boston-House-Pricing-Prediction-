c17b51ff601cd5e6fe7636b1e0d55fefd46cd2fc
SREEHARI RAJAMOHAN <sreehari32@gmail.com>
2024-02-16 16:10:33 +0530

    This commit includes app.py and python notebook

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Boston-House-Pricing-Prediction-
+### Boston-House-Pricing-Prediction-
+
+
+### Softwares and Tools Required
+1. [GITHUB](https://github.com)
+
+2. [HerokuAccount](https://heroku.com)
+
+3. [VSCodeIDE]( https:code.visualstudio.com/)
+
+4. [GITCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Command-Line)
+
+
+Create a New Environment 
+
+'''
+conda create -p venv  -y
+
+'''
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request,app,jsonify
+
+import pickle 
+
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+# Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalaar = pickle.load(open('scalarmodel.pkl','rb'))
+
+@app.route("/") 
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json(['data'])
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalaar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.prediction(new_data)
+    print(output[0])
+    return jsonify(output[0]) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
